5b4a960da97c3383615180c157dd5575cd5beed4
natsuno <linsukida@gmail.com>
2026-04-23 18:06:17 +0800

    配置文件载入，日志模块配置

--- tests/utils_logger_test.py
+++ tests/utils_logger_test.py
@@ -1,13 +1,5 @@
-# tests/utils_logger_test.py
-import sys
-from pathlib import Path
-
-# 这一行是关键！把项目根目录加入 Python 路径
-sys.path.insert(0, str(Path(__file__).parent.parent))
-
-# 现在可以正常导入了
 from src.utils.logger import logger
 
 logger = logger.bind(name = "utils_logger_test")
-# 测试
+
 logger.info("日志测试")
